651b676cbadabd7f8670ca74908f438702057544
scribu <mail@scribu.net>
2014-05-01 02:46:56 +0300

    extra points for style

--- Seats.hs
+++ Seats.hs
@@ -14,13 +14,13 @@ import Data.List.Split
 type SeatNr = Int
 type Seat = (SeatNr, Bool)
 type Compartment = [Seat]
-data Coach = Coach { seats :: [Seat], seatsPerComp :: Int } deriving (Show)
+data Coach = Coach { seats :: [Seat], compSize :: Int } deriving (Show)
 
 emptyCoach :: Int -> Int -> Coach
-emptyCoach numComp seatsPerComp = Coach [(i, False) :: Seat | i <- [1..numComp*seatsPerComp]] seatsPerComp
+emptyCoach numComp compSize = Coach [(i, False) | i <- [1..numComp*compSize]] compSize
 
 compartments :: Coach -> [Compartment]
-compartments (Coach seats seatsPerComp) = chunksOf seatsPerComp seats
+compartments (Coach seats compSize) = chunksOf compSize seats
 
 showSeat :: Seat -> [Char]
 showSeat (nr, occupied) = printf "%02d %s" nr (if occupied then "Y" else "N")
@@ -32,8 +32,9 @@ showCoach :: Coach -> [Char]
 showCoach coach = intercalate "\n----\n" $ map showCompartment $ compartments coach
 
 emptySeats :: [Seat] -> [SeatNr]
-emptySeats seats = foldr (\(nr, occupied) acc -> if occupied then acc else nr:acc)
-    [] seats
+emptySeats seats = foldr fn [] seats
+    where fn (_, True) acc = acc
+          fn (nr, False) acc = nr:acc
 
 splitNr :: Int -> Int -> [Int]
 splitNr nr partition = take (nr `div` partition) (repeat partition) ++
@@ -41,28 +42,30 @@ splitNr nr partition = take (nr `div` partition) (repeat partition) ++
     where remainder = nr `mod` partition
 
 goForward :: [Compartment] -> [Int] -> [SeatNr] -> ([Int], [SeatNr])
-goForward comp [] seats = ([], seats)
+goForward _ [] seats = ([], seats)
 goForward [] toBuy seats = (toBuy, seats)
 goForward (comp:ct) (toBuy:leftToBuy) markedSeats =
     let foundSeats = emptySeats comp in
-    if (toBuy <= length foundSeats) then
-        goForward ct leftToBuy (markedSeats ++ (take toBuy foundSeats))
+    if toBuy <= length foundSeats then
+        goForward ct leftToBuy $ markedSeats ++ (take toBuy foundSeats)
     else
         goForward ct (toBuy:leftToBuy) markedSeats
 
 findEmptySeats :: Coach -> Int -> [SeatNr]
 findEmptySeats coach seatsToBuy =
-    let seatChunks = (splitNr seatsToBuy (seatsPerComp coach))
+    let seatChunks = (splitNr seatsToBuy (compSize coach))
         (notBought, foundSeats) = goForward (compartments coach) seatChunks []
     in foundSeats ++ take (sum notBought) ((emptySeats $ seats coach) \\ foundSeats)
 
 occupySeats :: Coach -> [SeatNr] -> Coach
-occupySeats (Coach seats seatsPerComp) seatsToMark = Coach (map matchSeat seats) seatsPerComp
+occupySeats (Coach seats compSize) seatsToMark = Coach (map matchSeat seats) compSize
     where matchSeat (nr, True) = (nr, True)
           matchSeat (nr, False) = (nr, elem nr seatsToMark)
 
 purchaseSeats :: Coach -> Int -> (Coach, Maybe [SeatNr])
-purchaseSeats coach seatsToBuy = if (length foundSeats) < seatsToBuy
-    then (coach, Nothing)
-    else (coach `occupySeats` foundSeats, Just foundSeats)
-    where foundSeats = findEmptySeats coach seatsToBuy
+purchaseSeats coach seatsToBuy =
+    let foundSeats = findEmptySeats coach seatsToBuy in
+    if length foundSeats < seatsToBuy then
+        (coach, Nothing)
+    else
+        (coach `occupySeats` foundSeats, Just foundSeats)
